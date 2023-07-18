8103b75be9655da5cc20c56ec0b9e13d30a297b0
Vikramaditya <awesomevikram3@gmail.com>
2023-07-18 22:04:18 +0530

    feat:added new project

---
+++ AroundTheWorld/src/components/Sidebar.jsx
@@ -0,0 +1,25 @@
+import Logo from "./Logo";
+import styles from "./Sidebar.module.css";
+import AppNav from "./AppNav";
+import { Outlet } from "react-router-dom";
+function Sidebar() {
+  return (
+    <div className={styles.sidebar}>
+    <Logo/>
+    <AppNav/>
+    <Outlet/>
+
+    <footer className={styles.footer}>
+        <p className={styles.copyright}>
+            &copy; copyright {new Date().getFullYear()}
+            by AroundTheWorld Inc.
+        </p>
+    </footer>
+    
+   
+
+    </div>
+  )
+}
+
+export default Sidebar;
---
+++ AroundTheWorld/src/pages/AppLayout.jsx
@@ -0,0 +1,20 @@
+import Sidebar from '../components/Sidebar';
+import PageNav from '../components/PageNav';
+import styles from './AppLayout.module.css'
+import Map from '../components/Map';
+
+function AppLayout() {
+  return (
+    <div className={styles.app}>
+   
+    <Sidebar/>   
+    <Map/>
+    
+    </div>
+    
+    
+  );
+}
+
+
+export default AppLayout
